f1acbae0ca2c0d8bad321857f432ed7539139bfc
Apoorv Agarwal <apoorvagarwal@Apoorvs-MacBook-Air.local>
2023-12-23 11:29:50 +0530

    my first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Container = () => {
+  return (
+    <div className="" style={{ height: "100vh", width: "100vw", background: 'red' }}>
+        <span>This is the container.</span>
+    </div>
+  );
+}
+
+export default Container;

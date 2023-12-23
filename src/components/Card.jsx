91dd7a54d1a29b58eadca59e9f2a67c41f1f3160
Apoorv Agarwal <apoorvagarwal@Apoorvs-MacBook-Air.local>
2023-12-23 12:06:25 +0530

    fixed api call

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-const Card = ({data}) => {
-
+const Card = ({data: cardData}) => {
     return (
         <li className="" style={{ height: "200px", width: "500px", background: 'green', display: "flex", flexDirection: "column", padding: "12px" }}>
             <div className="header"  style={{ display: "flex", gap: "12px", alignItems: "center", height: "max-content", backgroundColor: "yellow" }}>
                 <div className="profile" style={{ height: "50px", width: "50px", borderRadius: "50%", background: "white" }}>
-
+                    <img style={{ height: "50px", width: "50px", borderRadius: "50%"}} src={cardData?.urlToImage} alt="profileImage" />
                 </div>
                 <div className="tileandauthor" style={{ display: "flex", direction: "column", alignItems: "center", height: "50px", background: "blue" }}>
-                    <span>Tile</span>
-                    <span>Author</span>
+                    <span>{cardData?.title}</span>
+                    <span>{cardData?.author}</span>
                 </div>
             </div>
-            <div className="body" style={{ paddin: "16px"}}>
-                <p></p>
+            <div className="body" style={{ padding: "16px"}}>
+                <p>{cardData?.content}</p>
             </div>
         </li>
     );
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -9,7 +9,7 @@ const Container = () => {
 
     const API_URI = `https://newsapi.org/v2/everything?q=tesla&from=2023-11-23&sortBy=publishedAt&apiKey=2f9486fc7d514ce7a52b4b02fde4a578`;
 
-    const fetchAPIURI  = async () => {
+    const fetchAPIURI = async () => {
         const cardDataResponse = fetch(API_URI)
         .then((response) => response.json())
         .then((data) => data)
@@ -20,44 +20,32 @@ const Container = () => {
     const fetchAndSetAPIURI = async () => {
         const fetchedData = await fetchAPIURI();
         
-        const cardDataMapper = fetchedData.map((cardData) => {
+        const cardDataMapper = fetchedData?.articles?.map((cardData) => {
             const dataObject = {
                 author: cardData?.author || "Apoorv",
                 content: cardData?.content,
                 description: cardData?.description,
                 publishedAt: cardData?.publishedAt,
                 source: cardData?.source,
-                title:
-                url:
-                urlToImage:
+                title: cardData?.title,
+                url:cardData?.url,
+                urlToImage:cardData?.urlToImage,
             }
+            return dataObject;
         })
-
         setCardData(cardDataMapper);
     }
-
-    useEffect(() => {
-        console.log("cardData", cardData);
-    }, [cardData])
 
 
     useEffect(() => {
        fetchAndSetAPIURI();
     }, [])
 
-    const demoList = [{
-        name: "apoorv",
-        profileName: "agarwal"
-    }, 
-    {
-        name: "rohit",
-        profileName: "Blah blah"
-    }]
-
     return (
         <ul className="" style={{ height: "100vh", width: "100vw", background: 'red', display: "flex", flexDirection: "column", alignItems: "center", padding: "16px", gap: "16px" }}>
             {
-                demoList.map((listElement) => <Card data={listElement}/>)
+               
+                cardData?.map((listElement, index) => <Card key ={`${listElement?.author}_${index}`} data={listElement}/>)
             }
         </ul>
     );
